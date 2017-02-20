8c466b4daaac5bb8cac3f118a4af6bebb9242fd3
hdd <1579752020@qq.com>
2017-02-20 15:57:12 +0800

    Update edit.js

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -1,44 +1,47 @@
 
-//监听返回事件
-window.onpopstate=function() {
+// 监听返回事件
+window.onpopstate = function () {
   urlQuery();
 };
 
-//获取列表
-(function() {
+// 获取列表
+(function () {
   urlQuery();
 })();
 
-//查找
-$(".find").click(function(){
-  var pram = $("#filterForm").serializeArray();
-  var query = $("#filterForm").serialize();
-  var url =  window.location.href.split("?")[0];
+// 查找
+$('.find').click(function () {
+  const pram = $('#filterForm').serializeArray();
+  const query = $('#filterForm').serialize();
+  const url =  window.location.href.split('?')[0];
   queryList(pram);
-  window.history.pushState({},"",url + "?" + encodeURI(query));
+  window.history.pushState({}, '', url + '?' + encodeURI(query));
 });
 
-//删除
-$(".j-userTable").on("click",".delete",function(){
-  var _this = this;
-  var speak=confirm("确定删除该条数据吗？");
-  if (speak==true){
-    //window.location.href = "/del?id="+ $(this).attr("_id");
+// 删除
+$('.j-userTable').on('click', '.delete', function() {
+  const _this = this;
+  const speak = confirm('确定删除该条数据吗？');
+  if (speak === true) {
+    // window.location.href = '/del?id='+ $(this).attr('_id');
     $.ajax({
-      url: "/api/del?id="+ $(this).attr("_id"),
-      dataType: "json",
+      url: '/api/del',
+      data: {
+        id: $(this).attr('_id'),
+      },
+      dataType: 'json',
       success: function(data){
-        if(data.flag){
-          $(_this).parents("tr").remove();
-        }else{
-          alert("删除失败，请待会重试");
+        if (data.flag){
+          $(_this).parents('tr').remove();
+        } else {
+          alert('删除失败，请待会重试');
         }
-      }
-    })
+      },
+    });
   }
 });
 
-//增加
+// 增加
 $('.j-addButton').click(function() {
   const person = $('#addForm').serialize();
   $.ajax({
@@ -63,119 +66,119 @@ $('.j-addButton').click(function() {
   });
 });
 
-//更新
-(function(){
-  var id;
-  $(".j-userTable").on("click",".update",function(){
-    var _this = this;
-    $("#updateDialog").modal("show");
-    id = $(_this).attr("_id");
+// 更新
+(function() {
+  let id;
+  $('.j-userTable').on('click','.update',function() {
+    const  _this = this;
+    $('#updateDialog').modal('show');
+    id = $(_this).attr('_id');
     $.ajax({
-      url:"/api/rewrite?id=" + id,
+      url:'/api/rewrite?id=' + id,
       type: 'post',
-      dataType: "json",
+      dataType: 'json',
       success: function (data) {
         if(data.flag) {
-          $("#updateForm input[type=text]:first").val(data.item[0].name);
-          $("#updateForm input[type=text]:eq(1)").val(data.item[0].sex);
-          $("#updateForm input[type=text]:eq(2)").val(data.item[0].age);
-          $("#updateForm input[type=text]:last").val(data.item[0].tel);
+          $('#updateForm input[type=text]:first').val(data.item[0].name);
+          $('#updateForm input[type=text]:eq(1)').val(data.item[0].sex);
+          $('#updateForm input[type=text]:eq(2)').val(data.item[0].age);
+          $('#updateForm input[type=text]:last').val(data.item[0].tel);
 
         }
-      }
+      },
     });
   });
   //
-  $(".j-updateButton").click(function(){
-    var person = $("#updateForm").serialize();
+  $('.j-updateButton').click(function () {
+    const person = $('#updateForm').serialize();
     $.ajax({
-      url:"/api/update?id="+ id,
+      url: '/api/update?id=' + id,
       data: person,
       type: 'post',
-      dataType: "json",
+      dataType: 'json',
       success: function (data) {
         if (data.flag) {
           queryList();
-          $("#updateDialog").modal("hide");
+          $('#updateDialog').modal('hide');
         } else {
-          alert("更新失败，请待会重试");
+          alert('更新失败，请待会重试');
         }
-      }
-    })
+      },
+    });
   });
 })();
 
-//获取数据的函数
-function queryList(pram){  
+// 函数获取数据
+function queryList(pram) {
   $.ajax({
-    url: "/api/edit",
-    type: "get",
-    dataType: "json",
+    url: '/api/edit',
+    type: 'get',
+    dataType: 'json',
     data: pram,
-    success: function(data){
-      if(data.flag){
-        var html = "";
-        if(!data.item.length){
-          html =  '<h2  style="text-align: center; margin-top: 100px;">'+'暂时无相关记录，请点击添加'+'</h2>'
-          $(".j-userTable tbody").empty();
-          $(".container-table .tips").empty().prepend(html);
-        }else{
-          for(var i= 0; i<data.item.length;i++ ){
-            html += '<tr>'+
-                '<td>'+data.item[i].name+'</td>'+
-                '<td>'+data.item[i].sex+'</td>'+
-                '<td>'+data.item[i].age+'</td>'+
-                '<td>'+data.item[i].tel+'</td>'+
-                '<td class="text-center">'+
-                '<a href="javascript:;" class="delete" _id="'+data.item[i]._id+'"> <input type="button" class="btn btn-style" value="删除"></a>&nbsp;&nbsp;'+
-                '<a href="javascript:;" class="update" _id="'+data.item[i]._id+'" > <input type="button" class="btn btn-style " value="更新"></a>'+
-                '</td>'+
+    success: function (data) {
+      if(data.flag) {
+        var html = '';
+        if(!data.item.length) {
+          html =  '<h2  style="text-align: center; margin-top: 100px;">' + '暂时无相关记录，请点击添加'+'</h2>'
+          $('.j-userTable tbody').empty();
+          $('.container-table .tips').empty().prepend(html);
+        } else {
+          for (let i = 0; i < data.item.length; i++) {
+            html += '<tr>' +
+                '<td>' + data.item[i].name + '</td>' +
+                '<td>' + data.item[i].sex + '</td>' +
+                '<td>' + data.item[i].age + '</td>' +
+                '<td>' + data.item[i].tel + '</td>' +
+                '<td class="text-center">' +
+                '<a href="javascript:;" class="delete" _id="' + data.item[i]._id+'"> <input type="button" class="btn btn-style" value="删除"></a>&nbsp;&nbsp;'+
+                '<a href="javascript:;" class="update" _id="' + data.item[i]._id+'" > <input type="button" class="btn btn-style " value="更新"></a>' +
+                '</td>' +
                 '</tr>'
-            $(".j-userTable tbody").empty().prepend(html);
+            $('.j-userTable tbody').empty().prepend(html);
           }
         }
-      }else{
-        alert("数据加载失败");
+      } else {
+        alert('数据加载失败');
       }
-    }
-  })
-}
+    },
+  });
+};
 
 
-//获取URL参数的函数
+// 获取URL参数的函数
 function GetQueryString(name) {
-  var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-  var r = window.location.search.substr(1).match(reg);
-  if(r!=null){
+  const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
+  const r = window.location.search.substr(1).match(reg);
+  if (r != null){
     return decodeURI(unescape(r[2]));
   }
   return null;
 }
 
-//URL查询函数
+// URL查询函数
 function urlQuery(){
   var pram = {},
-      _name = GetQueryString("name"),
-      _sex = GetQueryString("sex"),
-      _age = GetQueryString("age"),
-      _tel = GetQueryString("tel");
+      _name = GetQueryString('name'),
+      _sex = GetQueryString('sex'),
+      _age = GetQueryString('age'),
+      _tel = GetQueryString('tel');
 
-  $("#filterForm input[name=name]").val(_name);
-  $("#filterForm input[name=sex]").val(_sex);
-  $("#filterForm input[name=age]").val(_age);
-  $("#filterForm input[name=tel]").val(_tel);
+  $('#filterForm input[name=name]').val(_name);
+  $('#filterForm input[name=sex]').val(_sex);
+  $('#filterForm input[name=age]').val(_age);
+  $('#filterForm input[name=tel]').val(_tel);
 
-  if(_name){
-    pram.name=_name;
+  if(_name) {
+    pram.name = _name;
   }
-  if(_sex){
-    pram.sex=_sex;
+  if(_sex) {
+    pram.sex = _sex;
   }
-  if(_age){
-    pram.age=_age;
+  if(_age) {
+    pram.age = _age;
   }
-  if(_tel){
-    pram.tel=_tel;
+  if(_tel) {
+    pram.tel = _tel;
   }
   queryList(pram);
 }
